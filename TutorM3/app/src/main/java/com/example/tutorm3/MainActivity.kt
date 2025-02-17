f0efdaf181a8b43f95e01c899fa06edc9f3b1c45
ReyKeyS <104489667+ReyKeyS@users.noreply.github.com>
2025-02-17 21:47:31 +0700

    add tutorm3

---
+++ TutorM3/app/src/main/java/com/example/tutorm3/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.tutorm3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    // Deklarasi semua komponen yang akan dipakai
+    private lateinit var txtNama: TextView
+    private lateinit var txtNRP: TextView
+    private lateinit var txtJurusan: TextView
+    private lateinit var txtHasil: TextView
+    private lateinit var btnSubmit: Button
+    private lateinit var btnPindah: Button
+    private lateinit var chkAktif: CheckBox
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Ambil semua komponen yang sudah dideklarasi dan simpan dalam variabel
+        txtNama = findViewById(R.id.txtNama)
+        txtNRP = findViewById(R.id.txtNRP)
+        txtJurusan = findViewById(R.id.txtJurusan)
+        txtHasil = findViewById(R.id.txtHasil)
+        btnSubmit = findViewById(R.id.btnSubmit)
+        btnPindah = findViewById(R.id.btnPindah)
+        chkAktif = findViewById(R.id.chkAktif)
+
+        // Tambahkan event onclick pada button
+        btnSubmit.setOnClickListener(){
+            var nama = txtNama.text.toString();
+            var nrp = txtNRP.text.toString();
+            var jurusan = txtJurusan.text.toString();
+            var status = if (chkAktif.isChecked) "Aktif" else "Non-aktif"
+            if (nama == "" || nrp == "" || jurusan == ""){
+                // Menampilkan toast (notifikasi sederhana) kepada user
+                Toast.makeText(this@MainActivity, "Pastikan semua field terisi!", Toast.LENGTH_LONG).show()
+                // Toast.makeText(context, message, durasi).show()
+                // Jangan lupa .show(), kalau lupa maka toast tidak akan muncul
+                // Toast.LENGTH_LONG bisa diganti dengan Toast.LENG_SHORT untuk mengganti durasi toast ditampilkan
+            }
+            else{
+                // concate text & variabel
+                var hasil = "Mahasiswa bernama " + nama + " dengan NRP " + nrp + " dari jurusan " + jurusan + " berstatus " + status
+                // Random angka, jangan lupa "import kotlin.random.Random"
+                // from - until (until tidak termasuk)
+                var rnd = Random(System.currentTimeMillis())
+                var rndAngka = rnd.nextInt(0, 10)
+                hasil = rndAngka.toString() + ". " + hasil
+
+                // Cara lain random lbh pendek
+                // var rnd2 = (0..10).random()
+
+                // set txtHasil value
+                txtHasil.text = hasil
+            }
+        }
+
+        btnPindah.setOnClickListener(){
+            // intent berfungsi untuk mempersiapkan activity lain yang akan dipanggil
+            val intent = Intent(this, KampusActivity::class.java)
+            // Intent(activity asal, activity tujuan)
+            // startActivity untuk menampilkan
+            startActivity(intent)
+        }
+    }
+}
