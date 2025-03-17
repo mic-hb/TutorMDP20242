4a66d07458d9ece3e1e819e95b917de29c3fdf02
jessicas16 <jessica.susanto16@gmail.com>
2025-03-17 08:35:58 +0700

    fix m4

--- TutorM3/app/src/main/java/com/example/tutorm3/MockDB.kt
+++ TutorM3/app/src/main/java/com/example/tutorm3/MockDB.kt
@@ -6,11 +6,11 @@ class MockDB {
     // disini mockDB digunakan untuk menyimpan data mahasiswa + function untuk menambahkan data mahasiswa
     companion object{
         val listMhs = mutableListOf<Mahasiswa>(
-//            Mahasiswa("220181000", "Andi", 18),
-//            Mahasiswa("220181000", "Abdi", 18),
-//            Mahasiswa("220111001", "Budi", 11),
-//            Mahasiswa("220101002", "Chandra", 12),
-//            Mahasiswa("220171003", "Danu", 17),
+            Mahasiswa("220181000", "Andi", 18),
+            Mahasiswa("220181000", "Abdi", 18),
+            Mahasiswa("220111001", "Budi", 11),
+            Mahasiswa("220101002", "Chandra", 12),
+            Mahasiswa("220171003", "Danu", 17),
         )
         fun addMahasiswa(m:Mahasiswa){
             listMhs.add(m)
